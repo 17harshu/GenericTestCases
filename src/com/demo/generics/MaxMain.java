53a151cb4f8c803639e50ad9fd5c2e6aac6e0dc2
17harshu <harshalanandas17@gmail.com>
2022-02-26 21:19:16 +0530

    added: max method with more than 3 methods

--- src/com/demo/generics/MaxMain.java
+++ src/com/demo/generics/MaxMain.java
@@ -3,7 +3,11 @@ package com.demo.generics;
 public class MaxMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to check max of three variables");
-		String x = "apple", y = "banana", z = "peach";
-		FindMaxValue.testMaximum(x, y, z);
+		String x = "apple", y = "banana", z = "chikoo", m = "dates";
+		Integer a = 25, b = 24, c = 23;
+		Float p = 1.23f, q = 2.2f, r = 0.2f;
+		FindMaxValue.<String>testMaximum(x, y, z, m);
+		FindMaxValue.<Integer>testMaximum(a, b, c);
+		FindMaxValue.<Float>testMaximum(p, q, r);
 	}
 }
